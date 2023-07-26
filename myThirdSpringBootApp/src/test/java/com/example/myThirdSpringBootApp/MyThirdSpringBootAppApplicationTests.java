32c2ca1a5911406ef5ae5ce230d2da5782bf58a3
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-26 14:09:49 +0200

    Create Spring Boot project

---
+++ myThirdSpringBootApp/src/test/java/com/example/myThirdSpringBootApp/MyThirdSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myThirdSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyThirdSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
