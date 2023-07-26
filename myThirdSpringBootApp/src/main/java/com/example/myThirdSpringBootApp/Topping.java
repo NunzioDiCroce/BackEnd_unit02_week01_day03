3c5af39ba3bdbf97d7eade10a294b839ceb7e0f1
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-26 14:12:27 +0200

    Create Topping.java

---
+++ myThirdSpringBootApp/src/main/java/com/example/myThirdSpringBootApp/Topping.java
@@ -0,0 +1,22 @@
+package com.example.myThirdSpringBootApp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Slf4j
+
+public class Topping {
+	protected String name;
+	protected double calories;
+	protected double price;
+
+}
