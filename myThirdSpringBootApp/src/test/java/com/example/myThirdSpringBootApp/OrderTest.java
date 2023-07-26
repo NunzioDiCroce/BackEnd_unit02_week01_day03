80731e54967c2c93816b37fa33398585020dff93
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-26 16:44:18 +0200

    Update OrderTest.java

--- myThirdSpringBootApp/src/test/java/com/example/myThirdSpringBootApp/OrderTest.java
+++ myThirdSpringBootApp/src/test/java/com/example/myThirdSpringBootApp/OrderTest.java
@@ -3,19 +3,26 @@ package com.example.myThirdSpringBootApp;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderTest {
 
 	@Test
 	public void calculateOrderPriceTest() {
 
-		Order order = new Order();
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(BeansConfiguration.class);
 
-		double calculateOrderPrice = order.calculateOrderPrice();
+		Order order01 = ctx.getBean("Order01", Order.class);
+		Order order02 = ctx.getBean("Order02", Order.class);
 
-		double calculateOrderPriceTest = 47.11;
+		double calculateOrderPrice01 = order01.calculateOrderPrice();
+		double calculateOrderPrice02 = order02.calculateOrderPrice();
 
-		assertEquals(calculateOrderPriceTest, calculateOrderPrice);
+		double calculateOrderPrice01Test = 47.11;
+		double calculateOrderPrice02Test = 51.11;
+
+		assertEquals(calculateOrderPrice01Test, calculateOrderPrice01);
+		assertEquals(calculateOrderPrice02Test, calculateOrderPrice02);
 
 	}
 
