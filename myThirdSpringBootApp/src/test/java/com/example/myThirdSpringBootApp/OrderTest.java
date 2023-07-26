67828f5b2c6be45a06bba2e8cd40905e3e70749b
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-26 15:00:55 +0200

    Update OrderTest.java

--- myThirdSpringBootApp/src/test/java/com/example/myThirdSpringBootApp/OrderTest.java
+++ myThirdSpringBootApp/src/test/java/com/example/myThirdSpringBootApp/OrderTest.java
@@ -1,7 +1,21 @@
 package com.example.myThirdSpringBootApp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class OrderTest {
 
 	Order order = new Order();
+
+	double calculateOrderPrice = order.calculateOrderPrice();
+
+	double calculateOrderPriceTest = 47.11;
+
+	@Test
+	public double calculateOrderPriceTest() {
+		assertEquals(calculateOrderPrice, calculateOrderPriceTest);
+		return calculateOrderPrice;
+	}
 
 }
