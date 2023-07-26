e4268926d8b15707434a6951ffd23bb2d5fc6a55
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-26 14:14:06 +0200

    Create Franchise.java

---
+++ myThirdSpringBootApp/src/main/java/com/example/myThirdSpringBootApp/Franchise.java
@@ -0,0 +1,21 @@
+package com.example.myThirdSpringBootApp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Slf4j
+
+public class Franchise {
+	protected String name;
+	protected double price;
+
+}
